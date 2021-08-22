c312365a21d213d4a5a80c77c1a4d7643ae3dbae
TERBOUCHE Hacene <hacene.terbouche@gmail.com>
2021-08-22 23:43:39 +0200

    model definition, training, main func

--- review_cl/process.py
+++ review_cl/process.py
@@ -29,14 +29,15 @@ classes = [-1, 0, 1]
 classes_map = {-1: 0, 0: 1, 1: 2}
 
 # tokenization model
-PRE_TRAINED_MODEL_NAME = 'roberta-base'
+PRE_TRAINED_MODEL_NAME = "roberta-base"
 
 # create the tokenizer to use based on pre trained model
-tokenizer = RobertaTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)s
+tokenizer = RobertaTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
+
 
 def list_arg(raw_value):
     """argparse type for a list of strings"""
-    return str(raw_value).split(',')
+    return str(raw_value).split(",")
 
 
 def to_sentiment(star_rating):
@@ -55,34 +56,18 @@ def convert_to_bert_input_ids(review, max_seq_length):
         add_special_tokens=True,
         max_length=max_seq_length,
         return_token_type_ids=False,
-        padding='max_length',
+        padding="max_length",
         return_attention_mask=True,
-        return_tensors='pt',
+        return_tensors="pt",
         truncation=True,
     )
 
-    return encode_plus['input_ids'].flatten().tolist()
+    return encode_plus["input_ids"].flatten().tolist()
 
 
-def _preprocess_file(file, balance_dataset, max_seq_length, prefix):
-    """Summary
-
-    Args:
-        file (TYPE): Description
-        balance_dataset (TYPE): Description
-        max_seq_length (TYPE): Description
-        prefix (TYPE): Description
-    """
-    print('file {}'.format(file))
-    print('balance_dataset {}'.format(balance_dataset))
-    print('max_seq_length {}'.format(max_seq_length))
-    print('prefix {}'.format(prefix))
-
-    # Create a feature group
-    # the Feature Group that was set in the main notebook cannot be passed here
-    # - it will be used later in the notebook for other purposes
-    # you need to create a Feature Group with the same Feature Definitions
-    # within the processing job
+def _preprocess_file(file, max_seq_length):
+    print("file {}".format(file))
+    print("max_seq_length {}".format(max_seq_length))
 
     # read file
     df = pd.read_csv(file, index_col=0)
@@ -90,215 +75,107 @@ def _preprocess_file(file, balance_dataset, max_seq_length, prefix):
     df.isna().values.any()
     df = df.dropna()
     df = df.reset_index(drop=True)
-    print('Shape of dataframe {}'.format(df.shape))
+    print("Shape of dataframe {}".format(df.shape))
 
     # convert star rating into sentiment
-    df['sentiment'] = df['Rating'].apply(
+    df["sentiment"] = df["Rating"].apply(
         lambda star_rating: to_sentiment(star_rating=star_rating)
     )
-    print('Shape of dataframe with sentiment {}'.format(df.shape))
+    print("Shape of dataframe with sentiment {}".format(df.shape))
 
     # convert sentiment (-1, 0, 1) into label_id (0, 1, 2)
-    df['label_id'] = df['sentiment'].apply(lambda sentiment: classes_map[sentiment])
-    print('df[label_id] after using classes_map: {}'.format(df['label_id']))
-
-    df['input_ids'] = df['Review Text'].apply(
+    df["label_id"] = df["sentiment"].apply(lambda sentiment: classes_map[sentiment])
+    df["input_ids"] = df["Review Text"].apply(
         lambda review: convert_to_bert_input_ids(review, max_seq_length)
     )
-    print(
-        'df[input_ids] after calling convert_to_bert_input_ids: {}'.format(
-            df['input_ids']
-        )
-    )
-
     # convert the index into a review_id
     df.reset_index(inplace=True)
-    df = df.rename(columns={'index': 'review_id', 'Review Text': 'review_body'})
+    df = df.rename(columns={"index": "review_id", "Review Text": "review_body"})
 
     # drop all columns except the following:
-    df = df[['review_id', 'sentiment', 'label_id', 'input_ids', 'review_body']]
+    df = df[["review_id", "sentiment", "label_id", "input_ids", "review_body"]]
     df = df.reset_index(drop=True)
+    print("Shape of dataframe after dropping columns {}".format(df.shape))
 
-    print(df.head())
-
-    print('Shape of dataframe after dropping columns {}'.format(df.shape))
+    return df
 
 
-def split_df(df, train_rate, validation_rate, balance_dataset):
+def split_df(
+    df, output_data, train_split_percentage, test_split_percentage, balance_dataset
+):
     # balance the dataset by sentiment down to the minority class
     if balance_dataset:
 
-        df_unbalanced_grouped_by = df.groupby('sentiment')
+        df_unbalanced_grouped_by = df.groupby("sentiment")
         df_balanced = df_unbalanced_grouped_by.apply(
             lambda x: x.sample(df_unbalanced_grouped_by.size().min()).reset_index(
                 drop=True
             )
         )
-        print('Shape of balanced df: {}'.format(df_balanced.shape))
-        print(df_balanced['sentiment'].head())
         df = df_balanced
 
-    # adding timestamp as date column
-    timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-    print(timestamp)
-
-    df['date'] = timestamp
-    print('Shape of df with date: {}'.format(df.shape))
-
-    # split dataset
-    print('Shape of dataframe before splitting {}'.format(df.shape))
-
-    print('train split percentage {}'.format(args.train_split_percentage))
-    print('validation split percentage {}'.format(args.validation_split_percentage))
-    print('test split percentage {}'.format(args.test_split_percentage))
-
-    holdout_percentage = 1.00 - args.train_split_percentage
-    print('holdout percentage {}'.format(holdout_percentage))
+    holdout_percentage = 1.00 - train_split_percentage
+    print("holdout percentage {}".format(holdout_percentage))
     df_train, df_holdout = train_test_split(
-        df, test_size=holdout_percentage, stratify=df['sentiment']
+        df, test_size=holdout_percentage, stratify=df["sentiment"]
     )
 
-    test_holdout_percentage = args.test_split_percentage / holdout_percentage
-    print('test holdout percentage {}'.format(test_holdout_percentage))
+    test_holdout_percentage = test_split_percentage / holdout_percentage
+    print("test holdout percentage {}".format(test_holdout_percentage))
     df_validation, df_test = train_test_split(
-        df_holdout, test_size=test_holdout_percentage, stratify=df_holdout['sentiment']
+        df_holdout, test_size=test_holdout_percentage, stratify=df_holdout["sentiment"]
     )
 
     df_train = df_train.reset_index(drop=True)
     df_validation = df_validation.reset_index(drop=True)
     df_test = df_test.reset_index(drop=True)
 
-    print('Shape of train dataframe {}'.format(df_train.shape))
-    print('Shape of validation dataframe {}'.format(df_validation.shape))
-    print('Shape of test dataframe {}'.format(df_test.shape))
-
-    train_data = '{}/sentiment/train'.format(args.output_data)
-    validation_data = '{}/sentiment/validation'.format(args.output_data)
-    test_data = '{}/sentiment/test'.format(args.output_data)
+    train_data = "{}/sentiment/train".format(output_data)
+    validation_data = "{}/sentiment/validation".format(output_data)
+    test_data = "{}/sentiment/test".format(output_data)
 
     ## write TSV Files
-    df_train.to_csv(
-        '{}/part-{}-{}.tsv'.format(
-            train_data, args.current_host, filename_without_extension
-        ),
-        sep='\t',
-        index=False,
-    )
-    df_validation.to_csv(
-        '{}/part-{}-{}.tsv'.format(
-            validation_data, args.current_host, filename_without_extension
-        ),
-        sep='\t',
-        index=False,
-    )
-    df_test.to_csv(
-        '{}/part-{}-{}.tsv'.format(
-            test_data, args.current_host, filename_without_extension
-        ),
-        sep='\t',
-        index=False,
-    )
-
-    # dataframe
-    df_train.head()
-    df_validation.head()
-    df_test.head()
-
-    column_names = [
-        'review_id',
-        'sentiment',
-        'date',
-        'label_id',
-        'input_ids',
-        'review_body',
-    ]
-
-    df_train_records = df_train[column_names]
-    df_train_records['split_type'] = 'train'
-    df_train_records.head()
-
-    df_validation_records = df_validation[column_names]
-    df_validation_records['split_type'] = 'validation'
-    df_validation_records.head()
-
-    df_test_records = df_test[column_names]
-    df_test_records['split_type'] = 'test'
-    df_test_records.head()
+    df_train.to_csv("{}.tsv".format(train_data), sep="\t", index=False)
+    df_validation.to_csv("{}.tsv".format(validation_data), sep="\t", index=False)
+    df_test.to_csv("{}.tsv".format(test_data), sep="\t", index=False)
 
 
 def parse_args():
-    # Unlike SageMaker training jobs (which have `SM_HOSTS` and `SM_CURRENT_HOST` env vars),
-    # processing jobs to need to parse the resource config file directly
-
-    # Local testing with CLI args
-    parser = argparse.ArgumentParser(description='Process')
-
+    parser = argparse.ArgumentParser(description="Process")
     parser.add_argument(
-        '--input-data', type=str, default='/opt/ml/processing/input/data'
+        "--input-data", type=str, default="data/womens_clothing_ecommerce_reviews.csv"
     )
-    parser.add_argument('--output-data', type=str, default='/opt/ml/processing/output')
-    parser.add_argument('--train-split-percentage', type=float, default=0.90)
-    parser.add_argument('--validation-split-percentage', type=float, default=0.05)
-    parser.add_argument('--test-split-percentage', type=float, default=0.05)
-    parser.add_argument('--balance-dataset', type=eval, default=True)
-    parser.add_argument('--max-seq-length', type=int, default=128)
+    parser.add_argument("--output-data", type=str, default="output_data")
+    parser.add_argument("--train-split-percentage", type=float, default=0.90)
+    parser.add_argument("--validation-split-percentage", type=float, default=0.05)
+    parser.add_argument("--test-split-percentage", type=float, default=0.05)
+    parser.add_argument("--balance-dataset", type=eval, default=True)
+    parser.add_argument("--max-seq-length", type=int, default=128)
     return parser.parse_args()
 
 
 def process(args):
 
-    preprocessed_data = '{}/sentiment'.format(args.output_data)
-    train_data = '{}/sentiment/train'.format(args.output_data)
-    validation_data = '{}/sentiment/validation'.format(args.output_data)
-    test_data = '{}/sentiment/test'.format(args.output_data)
-
-    # partial functions allow to derive a function with some parameters to a function with fewer parameters
-    # and fixed values set for the more limited function
-    # here 'preprocess_file' will be more limited function than '_preprocess_file' with fixed values for some parameters
-    preprocess_file = functools.partial(
-        _preprocess_file,
-        balance_dataset=args.balance_dataset,
-        max_seq_length=args.max_seq_length,
+    preprocessed_data = "{}/sentiment".format(args.output_data)
+    df = _preprocess_file(file=args.input_data, max_seq_length=args.max_seq_length)
+    split_df(
+        df,
+        args.output_data,
+        args.train_split_percentage,
+        args.test_split_percentage,
+        args.balance_dataset,
     )
 
-    input_files = glob.glob('{}/*.csv'.format(args.input_data))
-
-    num_cpus = multiprocessing.cpu_count()
-    print('num_cpus {}'.format(num_cpus))
-
-    p = multiprocessing.Pool(num_cpus)
-    p.map(preprocess_file, input_files)
-
-    print('Listing contents of {}'.format(preprocessed_data))
+    print("Listing contents of {}".format(preprocessed_data))
     dirs_output = os.listdir(preprocessed_data)
     for file in dirs_output:
         print(file)
 
-    print('Listing contents of {}'.format(train_data))
-    dirs_output = os.listdir(train_data)
-    for file in dirs_output:
-        print(file)
-
-    print('Listing contents of {}'.format(validation_data))
-    dirs_output = os.listdir(validation_data)
-    for file in dirs_output:
-        print(file)
-
-    print('Listing contents of {}'.format(test_data))
-    dirs_output = os.listdir(test_data)
-    for file in dirs_output:
-        print(file)
-
-    print('Complete')
+    print("Complete")
 
 
 if __name__ == "__main__":
 
     args = parse_args()
-    print('Loaded arguments:')
-    print(args)
-
-    print('Environment variables:')
-    print(os.environ)
-
+    print("Loaded arguments:")
     process(args)
